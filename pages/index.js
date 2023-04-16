2f34cb71625049d32586577caf7403e873c5e9eb
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2023-04-16 10:16:29 +0600

    google provider added successfully

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex justify-center items-center ">
+        <div className="bg-white p-3 rounded">
+          <button onClick={() => signIn('google')} className="text-red-500 ">Login with google</button>
+
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="bg-blue-900">
-      <h1 className="text-red-500">Test</h1>
-    </div>
+    <div>Logged in{session.user.email}</div>
   )
+
 }
